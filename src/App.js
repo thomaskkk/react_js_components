ee99424b099066d6a75a5d38e99b38d9a365babb
Thomas Kuryura <thomas.adm@gmail.com>
2023-10-04 11:05:50 -0300

    IS-11 adding uuid to teams

--- src/App.js
+++ src/App.js
@@ -3,35 +3,43 @@ import Banner from './components/Banner';
 import Form from './components/Form';
 import Team from './components/Team';
 import Footer from './components/Footer';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 
     const [teams, setTeams] = useState([
         {
+            id: uuidv4(),
             name: 'Programação',
             color: '#57C278'
         },
         {
+            id: uuidv4(),
             name: 'Front-End',
             color: '#82CFFA'
         },
         {
+            id: uuidv4(),
             name: 'Data Science',
             color: '#A6D157'
         },
         {
+            id: uuidv4(),
             name: 'Devops',
             color: '#E06B69'
         },
         {
+            id: uuidv4(),
             name: 'UX e Design',
             color: '#DB6EBF'
         },
         {
+            id: uuidv4(),
             name: 'Mobile',
             color: '#FFBA05'
         },
         {
+            id: uuidv4(),
             name: 'Inovação e Gestão',
             color: '#FF8A29'
         }
@@ -43,9 +51,9 @@ function App() {
         console.log('deletando colaborador')
     }
 
-    function changeTeamColor(color, name) {
+    function changeTeamColor(color, id) {
         setTeams(teams.map(team => {
-            if (team.name === name) {
+            if (team.id === id) {
                 team.color = color
             }
             return team
--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -5,7 +5,7 @@ import hexToRgba from 'hex-to-rgba';
 const Team = ({ team, coworkers, onDelete, changeColor }) => {
     return (
         (coworkers.length > 0) && <section className='team' style={{ backgroundImage: 'url(/img/fundo.png)', backgroundColor: hexToRgba(team.color, '0.6') }}>
-            <input onChange={event => changeColor(event.target.value, team.name)} value={team.color} type="color" className='input-color' />
+            <input onChange={event => changeColor(event.target.value, team.id)} value={team.color} type="color" className='input-color' />
             <h3 style={{ borderColor: team.color }}>{team.name}</h3>
             <div className='coworkers'>
                 {coworkers.map((coworker, index) => {
